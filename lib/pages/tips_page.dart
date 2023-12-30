b1253402a5a937b9f83ed85cce11c1aebfab5635
beem9 <humoudalghanim1@gmail.com>
2023-12-30 17:57:20 +0300

    just stashing

--- lib/pages/tips_page.dart
+++ lib/pages/tips_page.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:meditation_app/providers/tips_provider.dart';
@@ -7,7 +5,7 @@ import 'package:meditation_app/widgets/dialog_tip.dart';
 import 'package:provider/provider.dart';
 
 class TipsScreen extends StatefulWidget {
-  TipsScreen({super.key});
+  TipsScreen({Key? key}) : super(key: key);
 
   @override
   State<TipsScreen> createState() => _TipsScreenState();
@@ -31,94 +29,99 @@ class _TipsScreenState extends State<TipsScreen> {
               );
             },
             icon: Icon(Icons.add),
-          )
+          ),
         ],
       ),
       body: FutureBuilder(
         future: context.read<TipProvider>().tipList(),
         builder: (context, snapshot) {
-          return Consumer<TipProvider>(
-            builder: (context, value, child) => ListView.builder(
-              itemCount: value.tipsList.length,
-              itemBuilder: (context, index) {
-                context.read<TipProvider>().tipList();
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error loading data'),
+            );
+          } else {
+            return Consumer<TipProvider>(
+              builder: (context, value, child) => ListView.builder(
+                itemCount: value.tipsList.length,
+                itemBuilder: (context, index) {
+                  final upvotesCount =
+                      value.tipsList[index].upvotes?.length ?? 0;
+                  final downvotesCount =
+                      value.tipsList[index].downvotes?.length ?? 0;
+                  final totalVotes = upvotesCount - downvotesCount;
 
-                return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                value.tipsList[index].text.toString(),
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(height: 8.0),
-                              Text(
-                                value.tipsList[index].author.toString(),
-                                style: TextStyle(fontSize: 14.0),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Row(
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                IconButton(
-                                  icon: Icon(Icons.arrow_upward),
-                                  onPressed: () {
-                                    // Upvote logic
-                                    context
-                                        .read<TipProvider>()
-                                        .upVouteTip(value.tipsList[index].id!);
-                                  },
-                                ),
                                 Text(
-                                  ""
-                                  //            value.tipsList[index].upvotes.length.toString(),
-
-                                  , // Replace this with the actual vote count
-                                  style: TextStyle(fontSize: 16),
+                                  value.tipsList[index].text.toString(),
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                                IconButton(
-                                  icon: Icon(Icons.arrow_downward),
-                                  onPressed: () {
-                                    context
-                                        .read<TipProvider>()
-                                        .downVoteTip(value.tipsList[index].id!);
-                                    // Downvote logic
-                                  },
+                                SizedBox(height: 8.0),
+                                Text(
+                                  value.tipsList[index].author.toString(),
+                                  style: TextStyle(fontSize: 14.0),
                                 ),
                               ],
                             ),
-                            IconButton(
-                              icon: Icon(Icons.delete),
-                              onPressed: () {
-                                setState(() {
-                                  context.read<TipProvider>().deleteTip(
-                                        value.tipsList[index].id!,
-                                      );
-                                });
-                              },
-                            ),
-                          ],
-                        ),
-                      ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Row(
+                                children: [
+                                  IconButton(
+                                    icon: Icon(Icons.arrow_upward),
+                                    onPressed: () {
+                                      context.read<TipProvider>().upVouteTip(
+                                          value.tipsList[index].id!);
+                                    },
+                                  ),
+                                  Text(
+                                    '$totalVotes',
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                  IconButton(
+                                    icon: Icon(Icons.arrow_downward),
+                                    onPressed: () {
+                                      context.read<TipProvider>().downVoteTip(
+                                          value.tipsList[index].id!);
+                                    },
+                                  ),
+                                ],
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.delete),
+                                onPressed: () {
+                                  setState(() {
+                                    context
+                                        .read<TipProvider>()
+                                        .deleteTip(value.tipsList[index].id!);
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              },
-            ),
-          );
+                  );
+                },
+              ),
+            );
+          }
         },
       ),
     );
