53c56aeed67cb3daef712e2dcdd82ef7c7bbb821
beem9 <humoudalghanim1@gmail.com>
2023-12-30 18:02:09 +0300

    tips page missing personal tips

--- lib/pages/tips_page.dart
+++ lib/pages/tips_page.dart
@@ -5,7 +5,7 @@ import 'package:meditation_app/widgets/dialog_tip.dart';
 import 'package:provider/provider.dart';
 
 class TipsScreen extends StatefulWidget {
-  TipsScreen({Key? key}) : super(key: key);
+  TipsScreen({super.key});
 
   @override
   State<TipsScreen> createState() => _TipsScreenState();
@@ -17,7 +17,7 @@ class _TipsScreenState extends State<TipsScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("Book app"),
+        title: Text("Tips"),
         actions: [
           IconButton(
             onPressed: () {
@@ -29,13 +29,12 @@ class _TipsScreenState extends State<TipsScreen> {
               );
             },
             icon: Icon(Icons.add),
-          ),
+          )
         ],
       ),
       body: FutureBuilder(
         future: context.read<TipProvider>().tipList(),
         builder: (context, snapshot) {
-<<<<<<< HEAD
           int likes = 1;
           return Consumer<TipProvider>(
             builder: (context, value, child) => ListView.builder(
@@ -54,36 +53,37 @@ class _TipsScreenState extends State<TipsScreen> {
                         ?.length ??
                     0;
                 dynamic totalVotes = upvotesCount - downvotesCount;
-=======
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error loading data'),
-            );
-          } else {
-            return Consumer<TipProvider>(
-              builder: (context, value, child) => ListView.builder(
-                itemCount: value.tipsList.length,
-                itemBuilder: (context, index) {
-                  final upvotesCount =
-                      value.tipsList[index].upvotes?.length ?? 0;
-                  final downvotesCount =
-                      value.tipsList[index].downvotes?.length ?? 0;
-                  final totalVotes = upvotesCount - downvotesCount;
->>>>>>> b1253402a5a937b9f83ed85cce11c1aebfab5635
 
-                  return Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                value.tipsList[index].text.toString(),
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 8.0),
+                              Text(
+                                value.tipsList[index].author.toString(),
+                                style: TextStyle(fontSize: 14.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Row(
                               children: [
-<<<<<<< HEAD
                                 IconButton(
                                   icon: Icon(Icons.arrow_upward),
                                   onPressed: () {
@@ -111,77 +111,37 @@ class _TipsScreenState extends State<TipsScreen> {
                                     context.read<TipProvider>().tipList();
                                     // Downvote logic
                                   },
-=======
-                                Text(
-                                  value.tipsList[index].text.toString(),
-                                  style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                SizedBox(height: 8.0),
-                                Text(
-                                  value.tipsList[index].author.toString(),
-                                  style: TextStyle(fontSize: 14.0),
->>>>>>> b1253402a5a937b9f83ed85cce11c1aebfab5635
                                 ),
                               ],
                             ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Row(
-                                children: [
-                                  IconButton(
-                                    icon: Icon(Icons.arrow_upward),
-                                    onPressed: () {
-                                      context.read<TipProvider>().upVouteTip(
-                                          value.tipsList[index].id!);
-                                    },
-                                  ),
-                                  Text(
-                                    '$totalVotes',
-                                    style: TextStyle(fontSize: 16),
-                                  ),
-                                  IconButton(
-                                    icon: Icon(Icons.arrow_downward),
-                                    onPressed: () {
-                                      context.read<TipProvider>().downVoteTip(
-                                          value.tipsList[index].id!);
-                                    },
-                                  ),
-                                ],
-                              ),
-                              IconButton(
-                                icon: Icon(Icons.delete),
-                                onPressed: () {
-                                  setState(() {
-                                    context
-                                        .read<TipProvider>()
-                                        .deleteTip(value.tipsList[index].id!);
-                                  });
-                                },
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+                            IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: () {
+                                setState(() {
+                                  context.read<TipProvider>().deleteTip(
+                                        value.tipsList[index].id!,
+                                      );
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  );
-                },
-              ),
-            );
-          }
+                  ),
+                );
+              },
+            ),
+          );
         },
       ),
     );
   }
 
-  likes(int ups, int down, index) {
-    int like = TipProvider().tipsList[index].upvotes!.length;
-    int dislikes = TipProvider().tipsList[index].downvotes!.length;
-    int total = like - dislikes;
-    return total;
-  }
+  // likes(int ups, int down, index) {
+  //   int like = TipProvider().tipsList[index].upvotes!.length;
+  //   int dislikes = TipProvider().tipsList[index].downvotes!.length;
+  //   int total = like - dislikes;
+  //   return total;
+  // }
 }
