78dbb48d9f49828d5da262ca9ae91fdf7126eb90
Alawita <alybinrajab@gmail.com>
2024-01-01 04:28:49 -0800

    internet connectivity

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,9 +5,10 @@ import 'package:meditation_app/providers/auth_provider.dart';
 import 'package:meditation_app/widgets/grid.dart';
 import 'package:meditation_app/widgets/user_drawer.dart';
 import 'package:provider/provider.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({super.key});
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -15,86 +16,122 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   User user = User(username: '', password: '');
+
+  // Check if the user is connected to the internet
+  Future<bool> _isConnected() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    return connectivityResult != ConnectivityResult.none;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
           IconButton(
-              onPressed: () {
+            onPressed: () async {
+              bool isConnected = await _isConnected();
+              if (isConnected) {
                 context.read<AuthProvider>().logOut();
                 GoRouter.of(context).goNamed("signin");
-              },
-              icon: Icon(Icons.logout))
+              } else {
+                // Display a message when not connected to the internet
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Please connect to the internet."),
+                    duration: Duration(seconds: 2),
+                  ),
+                );
+              }
+            },
+            icon: Icon(Icons.logout),
+          )
         ],
         title: Text('Profile Page'),
       ),
       drawer: UserDrawer(
-          username: context.read<AuthProvider>().getUserFromToken().username),
+        username: context.read<AuthProvider>().getUserFromToken().username,
+      ),
       body: GridView(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, mainAxisSpacing: 10),
+          crossAxisCount: 2,
+          mainAxisSpacing: 10,
+        ),
         children: [
           GridItem(
-              image: "assets/images/tips.png",
-              title: "Tips",
-              onTap: () {
-                GoRouter.of(context).pushNamed('tips');
-              }),
+            image: "assets/images/tips.png",
+            title: "Tips",
+            onTap: () async {
+              bool isConnected = await _isConnected();
+              if (isConnected) {
+                GoRouter.of(context).pushNamed("tips");
+              } else {
+                // Display a message when not connected to the internet
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Please connect to the internet."),
+                    duration: Duration(seconds: 2),
+                  ),
+                );
+              }
+            },
+          ),
           GridItem(
-              image: "assets/images/videos.png",
-              title: "Yoga videos",
-              onTap: () {
-                GoRouter.of(context).pushNamed('exer');
-              }),
+            image: "assets/images/videos.png",
+            title: "Yoga videos",
+            onTap: () async {
+              bool isConnected = await _isConnected();
+              if (isConnected) {
+                GoRouter.of(context).pushNamed("exer");
+              } else {
+                // Display a message when not connected to the internet
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Please connect to the internet."),
+                    duration: Duration(seconds: 2),
+                  ),
+                );
+              }
+            },
+          ),
           GridItem(
-              image: "assets/images/music.png",
-              title: "Music",
-              onTap: () {
+            image: "assets/images/music.png",
+            title: "Music",
+            onTap: () async {
+              bool isConnected = await _isConnected();
+              if (isConnected) {
                 GoRouter.of(context).pushNamed("music");
-              }),
+              } else {
+                // Display a message when not connected to the internet
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Please connect to the internet."),
+                    duration: Duration(seconds: 2),
+                  ),
+                );
+              }
+            },
+          ),
           GridItem(
-              image: "assets/images/med.png",
-              title: "Meditation",
-              onTap: () {
+            image: "assets/images/med.png",
+            title: "Meditation",
+            onTap: () async {
+              bool isConnected = await _isConnected();
+              if (isConnected) {
                 GoRouter.of(context).pushNamed("med");
-              })
+              } else {
+                // Display a message when not connected to the internet
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Please connect to the internet."),
+                    duration: Duration(seconds: 2),
+                  ),
+                );
+              }
+            },
+          )
         ],
       ),
     );
   }
 }
-
-// void _showUserProfile(BuildContext context, User user) {
-//   showDialog(
-//     context: context,
-//     builder: (BuildContext context) {
-//       return AlertDialog(
-//         title: Text('User Details'),
-//         content: Column(
-//           mainAxisSize: MainAxisSize.min,
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: <Widget>[
-//             Center(
-//               child: CircleAvatar(
-//                 radius: 50,
-//                 //backgroundImage: user.picture as ImageProvider,
-//               ),
-//             ),
-//             SizedBox(height: 10),
-//             Text(
-//                 'Username: ${context.read<AuthProvider>().getUsernameFromToken()}'),
-//           ],
-//         ),
-//         actions: <Widget>[
-//           TextButton(
-//             onPressed: () {
-//               Navigator.of(context).pop();
-//             },
-//             child: Text('Close'),
-//           ),
-//         ],
-//       );
-//     },
-//   );
-// }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
 import file_selector_macos
 import package_info_plus
 import path_provider_foundation
@@ -14,6 +15,7 @@ import video_player_avfoundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
